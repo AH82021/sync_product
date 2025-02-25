79371d525d0f226508d8b0836306b738976b7be2
ajmal-hakim <ajmal.hakim@va.gov>
2025-02-25 13:18:51 -0500

    [wt-12323] add a simple method and test it

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -4,11 +4,22 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class AppTest {
 
     @Test
     void testAppInitialization() {
         // Test the main method initialization
         App.main(new String[]{});
+    }
+
+    @Test
+
+    void  testCheckUsername() {
+        App app = new App();
+        assertTrue(app.checkUsername("username123"), "Username should be more than 8 characters");
+        assertFalse(app.checkUsername("user"), "Username should be less than 8 characters");
     }
 }
